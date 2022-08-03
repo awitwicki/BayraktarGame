136e2a3f74678f1061ec77c5077ce5ecc96c837c
Vortex784 <109910240+Vortex784@users.noreply.github.com>
2022-08-03 10:58:17 +0200

    Score + codestyle

--- docs/script.js
+++ docs/script.js
@@ -1,53 +1,52 @@
-
 function startGame() {
     document.getElementById("start").style.visibility = "hidden";
     document.getElementById("menu").style.visibility = "hidden";
     document.getElementById("menuBack").remove();
     gameArea.start();
     startLevel();
-}
-
-var tank;
-var drone;
-var rocket;
-var cooldown = false;
-var cooldownTimer = 0;
-var offset;
-var rocketLaunched = false;
-var score = 0;
-
-const droneImageUrl =  "res/Drone.png";
-const tankImageUrl = "res/Tank.png";
-const tankDestroyedImageUrl = "res/Tank_Destroyed.png";
-const targetImageUrl = "res/Target.png";
-const rocketImageUrl = "res/Rocket.png";
-
-var gameArea = {
+  }
+  
+  var tank;
+  var drone;
+  var rocket;
+  var cooldown = false;
+  var cooldownTimer = 0;
+  var offset;
+  var rocketLaunched = false;
+  var score = 0;
+  
+  const droneImageUrl =  "res/Drone.png";
+  const tankImageUrl = "res/Tank.png";
+  const tankDestroyedImageUrl = "res/Tank_Destroyed.png";
+  const targetImageUrl = "res/Target.png";
+  const rocketImageUrl = "res/Rocket.png";
+  
+  var gameArea = {
     start: function () {
-        var cv = document.getElementById('cv');
-        cv.style.visibility = "visible";
-        if (window.innerWidth < 700) {
-            cv.width = window.innerWidth;
-        }
-        else {
-            cv.width = 700;
-        }
-
-        this.context = cv.getContext("2d");
-
-        if (window.innerHeight < 950) {
-            cv.height = window.innerHeight;
-        }
-        else {
-            cv.height = 950;
-        }
+      var cv = document.getElementById('cv');
+      cv.style.visibility = "visible";
+      if (window.innerWidth < 700) {
+        cv.width = window.innerWidth;
+      }
+      else {
+        cv.width = 700;
+      }
+  
+      this.context = cv.getContext("2d");
+  
+      if (window.innerHeight < 950) {
+        cv.height = window.innerHeight;
+      }
+      else {
+        cv.height = 950;
+      }
     },
     clear: function () {
-        this.context.clearRect(0, 0, cv.width, cv.height);
-      }
-}
-
-function component(width, height, color, x, y, type) {
+      this.context.clearRect(0, 0, cv.width, cv.height);
+    }
+  }
+  
+  function component(width, height, color, x, y, type) {
     this.type = type;
   
     this.image = new Image();
@@ -64,69 +63,63 @@ function component(width, height, color, x, y, type) {
     this.destroyed = false;
   
     this.update = function () {
-        var ctx = gameArea.context;
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(this.angle);
-        ctx.drawImage(this.image, this.width / -2 , this.height / -2, this.width, this.height);
-        ctx.restore();
+      var ctx = gameArea.context;
+      ctx.save();
+      ctx.translate(this.x, this.y);
+      ctx.rotate(this.angle);
+      ctx.drawImage(this.image, this.width / -2, this.height / -2, this.width, this.height);
+      ctx.restore();
     }
     this.newTankPos = function () {
-        this.y += 1;
+      this.y += 1;
     }
     this.newRocketPos = function () {
-        this.x += this.speedX;
-        this.y += this.speedY;
+      this.x += this.speedX;
+      this.y += this.speedY;
     }
-    // this.dronePos = function () {
-    //     cv.addEventListener('mousemove', (e) => {
-    //     this.x = e.clientX;
-    //     }
-    //     )   
-    // }
-}
-
-// tankArray[]
-var isGameOver = false;
-var isLineCrossed = false;
-function startLevel() {
+  }
+  
+  // tankArray[]
+  var isGameOver = false;
+  var isLineCrossed = false;
+  function startLevel() {
     initLevel();
-
-    if(!isGameOver) {
+  
+    if (!isGameOver) {
       setInterval(updateGameArea, 10)
     }
-}
-
-function initLevel() {
+  }
+  
+  function initLevel() {
     // Randon number (0..this.canvas.width)
-
+  
     // Create new tank object with speed vector and put to tankArray[]
     tankArray = [];
     offset = (window.innerWidth - cv.width) / 2;
-
-    cv.addEventListener ('click', (evt) => {});
+  
+    cv.addEventListener('click', (evt) => { });
     cv.onclick = (evt) => {
-        if(!cooldown) { 
-            target.x = evt.clientX - offset;
-            target.y = evt.clientY ;
-            rocket.x = drone.x;
-            rocket.y = drone.y;
-            rocket.speedX = (target.x - drone.x)/50;
-            rocket.speedY = (target.y - drone.y)/50;
-            cooldown = true;
-            rocketLaunched = true;
-            rocket.angle = Math.atan2(target.y - rocket.y, target.x - rocket.x ); 
-        }
+      if (!cooldown) {
+        target.x = evt.clientX - offset;
+        target.y = evt.clientY;
+        rocket.x = drone.x;
+        rocket.y = drone.y;
+        rocket.speedX = (target.x - drone.x) / 50;
+        rocket.speedY = (target.y - drone.y) / 50;
+        cooldown = true;
+        rocketLaunched = true;
+        rocket.angle = Math.atan2(target.y - rocket.y, target.x - rocket.x);
+      }
     }
-    
+  
     tank = new component(100, 100, tankImageUrl, Math.random() * (cv.width - 50) , -100, "tank");
     drone = new component(100, 100, droneImageUrl, cv.width/2, cv.height/1.1, "drone" );
     rocket = new component (25, 25, rocketImageUrl, -100, -100, "rocket");
     target = new component (100, 100, targetImageUrl, -100, -100, "target");
-}
-
-// Update game tick
-function updateGameArea() {
+  }
+  
+  // Update game tick
+  function updateGameArea() {
     gameArea.clear();
     drawLine();
     tank.update();
@@ -139,73 +132,72 @@ function updateGameArea() {
     checkLine();
     updateTimer();
     updateScore();
-}
-
-function checkLine() {
-    if(tank.y>=cv.height/1.2 && tank.destroyed == false){
-        endGame();
+  }
+  
+  function checkLine() {
+    if (tank.y >= cv.height / 1.2 && tank.destroyed == false) {
+      endGame();
     }
-}
-
-function endGame() {
+  }
+  
+  function endGame() {
     tank.y = +50;
     alert("Game Over");
     document.location.reload();
-}
-
-function drawLine() {
+  }
+  
+  function drawLine() {
     var ctx = cv.getContext("2d");
     ctx.strokeStyle = 'yellow';
     ctx.lineWidth = 5;
     ctx.beginPath();
-    ctx.moveTo(0, cv.height/1.2);
-    ctx.lineTo(cv.width, cv.height/1.2);
+    ctx.moveTo(0, cv.height / 1.2);
+    ctx.lineTo(cv.width, cv.height / 1.2);
     ctx.stroke();
-}
-
-function updateScore() {
+  }
+  
+  function updateScore() {
     var ctx = cv.getContext("2d");
     ctx.font = "30px Arial";
     ctx.fillText("Score: "+score, 10, 50)
-}
-
-function updateTimer() {
+  }
+  
+  function updateTimer() {
     cooldownTimer += 10;
     if (cooldownTimer >= 3000) {
-        cooldown = false;
-        cooldownTimer = 0;
+      cooldown = false;
+      cooldownTimer = 0;
     }
-}
-
-function checkRocket() {
+  }
+  
+  function checkRocket() {
     if (rocketLaunched) {
-        rocket.newRocketPos();
+      rocket.newRocketPos();
     }
-    if (rocket.x <= target.x + 5  && rocket.y <= target.y + 5 ) {
-    rocketLaunched = false;
-        if(50 > target.x - tank.x && target.x - tank.x > -50 && 50 > target.y - tank.y && target.y - tank.y > -50 && !tank.destroyed ){
-            updateTankStatus();
-        }
-        
-    rocket.x = -100;
-    rocket.y = -100;
-    target.x = -100;
-    target.y = -100;
+    if (rocket.x <= target.x + 5 && rocket.y <= target.y + 5) {
+      rocketLaunched = false;
+      if (50 > target.x - tank.x && target.x - tank.x > -50 && 50 > target.y - tank.y && target.y - tank.y > -50 && !tank.destroyed) {
+        updateTankStatus();
+      }
+      rocket.x = -100;
+      rocket.y = -100;
+      target.x = -100;
+      target.y = -100;
     }
-}
-
-function checkTankRespawn() {
+  }
+  
+  function checkTankRespawn() {
     if (tank.y > cv.height + 100) {
     tank.y = -100;
     tank.x = Math.random() * (cv.width - 50);
     tank.destroyed = false;
     tank.image.src = tankImageUrl;
-    }
-}
-
-function updateTankStatus() {
+      }
+  }
+  
+  function updateTankStatus() {
     tank.destroyed = true;
     tank.image.src = tankDestroyedImageUrl;
     score += 1;
-}
-     
+  }
+  
