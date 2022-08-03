2135ffceec8e157d644c9ed0ead3b4c1ad834db8
Vortex784 <109910240+Vortex784@users.noreply.github.com>
2022-08-03 15:14:22 +0200

    Multiple rockets support

--- docs/script.js
+++ docs/script.js
@@ -9,10 +9,10 @@ function startGame() {
   var tank;
   var drone;
   var rocket;
+  var target;
   var cooldown = false;
   var cooldownTimer = 0;
   var offset;
-  var rocketLaunched = false;
   var score = 0;
   var tankArray = [];
   var rockets = [];
@@ -64,6 +64,7 @@ function startGame() {
     this.speedY = 0;
     this.angle = 0;
     this.destroyed = false;
+    this.launched = false;
   
     this.update = function () {
       var ctx = gameArea.context;
@@ -105,22 +106,24 @@ function startGame() {
     cv.addEventListener('click', (evt) => { });
     cv.onclick = (evt) => {
       if (!cooldown) {
-        target.x = evt.clientX - offset;
-        target.y = evt.clientY;
-        rocket.x = drone.x;
-        rocket.y = drone.y;
-        rocket.speedX = (target.x - drone.x) / -((target.y - drone.y)/4) ;
-        rocket.speedY = (target.y - drone.y) / -((target.y - drone.y)/4) ;
+        var _rocket = new component (25, 25, rocketImageUrl, -100, -100, "rocket");
+        var _target = new component (100, 100, targetImageUrl, evt.clientX - offset, evt.clientY, "target");
+        _rocket.x = drone.x;
+        _rocket.y = drone.y;
+        _rocket.speedX = (_target.x - drone.x) / -((_target.y - cv.height-100)/6) ;
+        _rocket.speedY = (_target.y - drone.y) / -((_target.y - cv.height-100)/6) ;
         cooldown = true;
-        rocketLaunched = true;
-        rocket.angle = Math.atan2(target.y - rocket.y, target.x - rocket.x);
+        _rocket.launched = true;
+        _rocket.angle = Math.atan2(_target.y - _rocket.y, _target.x - _rocket.x);
+        rockets.push(_rocket);
+        targets.push(_target);
       }
     }
   
     //tank = new component(100, 100, tankImageUrl, Math.random() * (cv.width - 50) , -100, "tank");
     drone = new component(100, 100, droneImageUrl, cv.width/2, cv.height/1.1, "drone" );
-    rocket = new component (25, 25, rocketImageUrl, -100, -100, "rocket");
-    target = new component (100, 100, targetImageUrl, -100, -100, "target");
+    //rocket = new component (25, 25, rocketImageUrl, -100, -100, "rocket");
+    //target = new component (100, 100, targetImageUrl, -100, -100, "target");
   }
   
   // Update game tick
@@ -130,18 +133,24 @@ function startGame() {
     updateTimer();
     updateScore()
     drone.update();
-    target.update();
-    rocket.update();
-    checkRocket();
-    for (var i = 0; i < tankArray.length; i++) {
-      tank = tankArray[i];
-      tank.update();
-      tank.newTankPos();
-      checkTankRespawn();
-      checkLine();
+    if (targets.length > 0) {
+        for (i = 0; i < targets.length; i++) {
+          target = targets[i];
+          rocket = rockets[i];
+          target.update();
+          rocket.update();
+          checkRocket();
+        }
     }
-
-  }
+    for (var i = 0; i < tankArray.length; i++) {
+        tank = tankArray[i];
+        tank.update();
+        tank.newTankPos();
+        checkTankRespawn();
+        checkLine();
+      }
+    }
+  
   
   function checkLine() {
     if (tank.y >= cv.height / 1.2 && tank.destroyed == false) {
@@ -180,24 +189,26 @@ function startGame() {
   }
   
   function checkRocket() {
-    if (rocketLaunched) {
+    if (rocket.launched) {
       rocket.newRocketPos();
     }
     if (rocket.x <= target.x + 5 && rocket.y <= target.y + 5) {
-      rocketLaunched = false;Array[i]
+      rocket.launched = false;
       for (var i = 0; i < tankArray.length; i++) {
         if (50 > target.x - tankArray[i].x && target.x - tankArray[i].x > -50 && 50 > target.y - tankArray[i].y && target.y - tankArray[i].y > -50 && !tankArray[i].destroyed) {
           updateTankStatus(i);
         }
       }
-      
-      rocket.x = -100;
-      rocket.y = -100;
-      target.x = -100;
-      target.y = -100;
-
+    //remove rocket and target
+    rocket.x = -100;
+    rocket.y = -100;
+    target.x = -100;
+    target.y = -100;  
+    rockets.pop;
+    targets.pop;
 
     }
+    
   }
   
   function checkTankRespawn() {
